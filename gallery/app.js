abee34d517326270372ba695230cc753370cdac0
Ameen <md.ameenullah.h@gmail.com>
2020-11-25 20:50:08 +0530

    added js

--- gallery/app.js
+++ gallery/app.js
@@ -7,3 +7,23 @@ function getElement(selection) {
     `Please check "${selection}" selector, no such element exists`
   );
 }
+
+function Gallery(element){
+  this.list = [...element.querySelectorAll('.img')];
+  this.modal = getElement('.modal');
+  this.modalImg = getElement('.main-img');
+  this.modalImages = getElement('.modal-images');
+  this.closeBtn = getElement('.close-btn');
+  this.closeBtn = getElement('.prev-btn');
+  this.nextBtn = getElement('.next-btn');
+  this.modalImages = getElement('.modal-images');
+
+}
+
+
+const nature = new Gallery(getElement('.nature'));
+const city = new Gallery(getElement('.city'));
+
+
+
+
