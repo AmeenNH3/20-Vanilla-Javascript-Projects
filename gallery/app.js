9fe240e63e7c7a27024a4b9159625121a3d8b550
Ameen <md.ameenullah.h@gmail.com>
2020-11-26 11:57:09 +0530

    gallery project completed

--- gallery/app.js
+++ gallery/app.js
@@ -9,17 +9,85 @@ function getElement(selection) {
 }
 
 function Gallery(element){
+  this.container = element;
   this.list = [...element.querySelectorAll('.img')];
   this.modal = getElement('.modal');
   this.modalImg = getElement('.main-img');
+  this.imageName = getElement('.image-name');
   this.modalImages = getElement('.modal-images');
   this.closeBtn = getElement('.close-btn');
-  this.closeBtn = getElement('.prev-btn');
+  this.prevBtn = getElement('.prev-btn');
   this.nextBtn = getElement('.next-btn');
   this.modalImages = getElement('.modal-images');
+  this.openModal = this.openModal.bind(this);
+  
+  this.closeModal = this.closeModal.bind(this);  
+  this.prevImage = this.prevImage.bind(this);  
+  this.nextImage = this.nextImage.bind(this);  
+  this.clickedImage = this.clickedImage.bind(this);
+
+  this.container.addEventListener('click', function(e){
+   
+    if(e.target.classList.contains('img')){
+      this.openModal(e.target,this.list);
+    }
+  }.bind(this));
 
 }
 
+Gallery.prototype.openModal = function(selectedImage,list){
+  this.setMainImage(selectedImage);
+  
+  this.modalImages.innerHTML = list.map(function(image){
+          return `<img src=${image.src}  title=${image.title}  class="${selectedImage.dataset.id === image.dataset.id? 'modal-img selected' :'modal-img'}" data-id=${image.dataset.id} alt=${image.alt}/>`;        
+  }).join('');
+  
+  this.modal.classList.add('open');
+
+  this.closeBtn.addEventListener('click',this.closeModal);
+  this.nextBtn.addEventListener('click',this.nextImage);
+  this.prevBtn.addEventListener('click',this.prevImage);
+  this.modalImages.addEventListener('click',this.clickedImage);
+};
+
+Gallery.prototype.setMainImage = function(selectedImage){
+  this.modalImg.src= selectedImage.src;
+  this.imageName.textContent = selectedImage.title;
+  
+};
+
+Gallery.prototype.closeModal = function(){
+  this.modal.classList.remove('open');
+  this.closeBtn.removeEventListener('click',this.closeModal);
+  this.nextBtn.removeEventListener('click',this.nextImage);
+  this.prevBtn.removeEventListener('click',this.prevImage);
+  this.modalImages.removeEventListener('click',this.clickedImage);
+
+}
+
+Gallery.prototype.nextImage = function(){
+  const selected = this.modalImages.querySelector('.selected');
+  const next = selected.nextElementSibling || this.modalImages.firstElementChild;
+  selected.classList.remove('selected');
+  next.classList.add('selected');
+  this.setMainImage(next);
+};
+Gallery.prototype.prevImage = function(){
+  const selected = this.modalImages.querySelector('.selected');
+  const prev = selected.previousElementSibling || this.modalImages.lastElementChild;
+  selected.classList.remove('selected');
+  prev.classList.add('selected');
+  this.setMainImage(prev);
+};
+
+Gallery.prototype.clickedImage = function(e){
+if(e.target.classList.contains('modal-img')){
+  const selected = this.modalImages.querySelector('.selected');
+  selected.classList.remove('selected');
+  this.setMainImage(e.target);
+  e.target.classList.add('selected');
+}
+}
 
 const nature = new Gallery(getElement('.nature'));
 const city = new Gallery(getElement('.city'));
